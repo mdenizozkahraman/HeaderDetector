77763f5b711e74d05d6c06b7dd0fcc87d1c8aec6
M. Deniz Özkahraman <mehmetdenizozkahraman@outlook.com>
2023-12-05 22:49:51 +0300

    Get headers

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import requests
 
+def func_get_headers(url):
+
+    response = requests.get(url)
+    headers = response.headers
+
+    print(headers)
+
+func_get_headers("https://www.google.com")
+
