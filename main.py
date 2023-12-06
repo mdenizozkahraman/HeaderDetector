5ec43c3264a06d2cafd31f12c931fb3b62182679
M. Deniz Özkahraman <mehmetdenizozkahraman@outlook.com>
2023-12-06 17:02:58 +0300

    Update function

--- main.py
+++ main.py
@@ -2,10 +2,55 @@ import requests
 
 def func_get_headers(url):
 
-    response = requests.get(url)
-    headers = response.headers
+    headers = requests.get(url).headers
 
-    print(headers)
+
+    # https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+    expectedHeaders = [
+        'Cache-Control',
+        'Connection',
+        'Date',
+        'Host',
+        'User-Agent',
+        'Accept',
+        'Authorization',
+        'Location',
+        'Server',
+        'Content-Type',
+        'Content-Length',
+        'Strict-Transport-Security',
+        'X-Content-Type-Options',
+        'X-Frame-Options',
+        'Content-Security-Policy',
+        'TESTTESTTEST'
+    ]
+
+    missingHeaders = []
+    ownedHeaders = []
+
+    for header in expectedHeaders:
+        if header not in headers:
+            missingHeaders.append(header)
+        else:
+            ownedHeaders.append(header)
+
+    # return headers.get("Cache-Control")
+    #return headers
+    print("Data: ", headers)
+    print()
+    print("Expected Headers: ", expectedHeaders)
+    print()
+    print("Missing Headers: ", missingHeaders)
+    print()
+    print("Owned Headers ", ownedHeaders)
+    print()
+    print(headers.get("Date"))
+    print()
+    print(headers.get("Connection"))
+
+
+
 
 func_get_headers("https://www.google.com")
+
 
